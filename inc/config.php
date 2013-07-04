343efd444fc711af4b732f515642fc8888a7bd6f
莫争 <gaoli.gl@taobao.com>
2013-07-04 20:31:35 +0800

    添加 Util 类

--- inc/config.php
+++ inc/config.php
@@ -1,8 +1,12 @@
 <?php
 
-//数据库类链接配置  
-$GLOBALS['db_host'] = "127.0.0.1";
-$GLOBALS['db_user'] = "root";
-$GLOBALS['db_pwd'] = "";
-$GLOBALS['db_name'] = "kissydoc";
+// 数据库类链接配置
+$GLOBALS['db_host']     = "127.0.0.1";
+$GLOBALS['db_user']     = "root";
+$GLOBALS['db_pwd']      = "";
+$GLOBALS['db_name']     = "kissydoc";
 $GLOBALS['db_tablepre'] = "";
+
+// GitHub 应用配置
+$GITHUB['client_id']     = "442b4d1abc1629ec9a7e";
+$GITHUB['client_secret'] = "";
